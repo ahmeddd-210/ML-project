23fb805188cf9465b33299a25f9dd624be7ebe54
Ahmed <77333793+ahmeddd-210@users.noreply.github.com>
2025-01-30 22:08:47 +0100

    regression.c

--- regression.c
+++ regression.c
@@ -8,7 +8,7 @@
 #define seuil 0.00001
 #define maximum 10000
 
-double predict(double x,double a,double b){
+double predict(double x,double a,double b){       /*Fonction pour calculer y_chapeau*/
     return a*x+b;
 }
 
@@ -16,6 +16,7 @@ int main(){
     srand(time(NULL));
     double x[N],y[N],y_pred[N],w;
     double a1,b1;
+    // Création du nuage de points
     printf("saisir a1:");
     scanf("%lf", &a1);
     printf("saisir b1:");
@@ -25,10 +26,11 @@ int main(){
         x[i]=(double)rand()/RAND_MAX;
         y[i]=(a1 * x[i]) + b1 + w;
     }
-
+    // initialisation des coefficients a et b
     double a = (double)rand()/RAND_MAX;
     double b = (double)rand()/RAND_MAX;
-    
+
+    // Descente de gradient
     double da,db;
     int j=0;
     while(j<maximum){
@@ -36,8 +38,8 @@ int main(){
         db=0.0;
         for(int i=0;i<N;i++){
             y_pred[i]=predict(x[i],a,b);
-            da += 2 * (y_pred[i] - y[i]) * x[i];
-            db += 2 * (y_pred[i] - y[i]);
+            da += 2 * (y_pred[i] - y[i]) * x[i];   /* calcul de la derivee de la fonction cout par rapport à a */
+            db += 2 * (y_pred[i] - y[i]);          /* calcul de la derivee de la fonction cout par rapport à b */
         }
         da/=N;
         db/=N;
@@ -48,6 +50,7 @@ int main(){
         }
         j++;
     }
+    // Affichage des valeurs de a et b prédites
     printf("Valeurs predites:\na = %f | b = %f\n", a, b);
     printf("%d",j);
     return 0;
